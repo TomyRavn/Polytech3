10532c536b183ec33f361184938e87a6136d9669
mcsilva21 <mcsilva21@naver.com>
2020-05-14 17:42:51 +0900

    20200514 자바복습

---
+++ mcsilva21/Java/Ex01.java
@@ -0,0 +1,18 @@
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		
+		int x = 10;
+		int y = 20;
+		int z = (++x);
+		int t = (y--);
+		int s = (++x) + (y--);
+		System.out.println(z);
+		System.out.println(y);
+		System.out.println(s);
+
+	}
+
+}
---
+++ mcsilva21/Java/Ex04.java
@@ -0,0 +1,10 @@
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		int value = 356;
+		System.out.println(value-56);
+
+	}
+
+}
