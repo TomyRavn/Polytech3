26617f550441b1d0c928db11f163fd66ecb923e3
mcsilva21 <mcsilva21@naver.com>
2020-06-09 15:37:43 +0900

    0609 과제

---
+++ mcsilva21/Spring/0609 과제/StudentListServlet.java
@@ -0,0 +1,33 @@
+package kr.ac.kopo.student;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import kr.ac.kopo.student.StudentVo;
+
+@WebServlet ("/student/list.do")
+public class StudentListServlet extends HttpServlet {
+   
+   StudentDaoJdbc studentDao = new StudentDaoJdbc();
+   
+   @Override
+   protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+      ArrayList<StudentVo> list = studentDao.selectStudentList();
+      
+      req.setAttribute("stuList", list);
+   
+      req.getRequestDispatcher("/WEB-INF/jsp/Student/StudentList.jsp").forward(req, resp);
+   }
+}
