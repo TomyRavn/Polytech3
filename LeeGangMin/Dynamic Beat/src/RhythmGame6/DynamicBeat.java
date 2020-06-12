060ace031e78ff0daded6b177a7ecf113d744273
TomyRavn <godqwer123@naver.com>
2020-06-12 21:01:46 +0900

    2020-06-12, 세번째 업로드(리듬게임 소스 업로드)

--- LeeGangMin/Dynamic Beat/src/RhythmGame6/DynamicBeat.java
+++ LeeGangMin/Dynamic Beat/src/RhythmGame6/DynamicBeat.java
@@ -16,28 +16,36 @@ public class DynamicBeat extends JFrame {
 
 	private Image screenImage;
 	private Graphics screenGraphic;
-	
-	//이미지 소스 업로드
+
+	// 이미지 소스 업로드
 	private ImageIcon exitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/exitButtonEntered.png"));
 	private ImageIcon exitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/exitButtonBasic.png"));
-	private ImageIcon startButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/startButtonEntered.png"));
+	private ImageIcon startButtonEnteredImage = new ImageIcon(
+			Main.class.getResource("../images/startButtonEntered.png"));
 	private ImageIcon startButtonBasicImage = new ImageIcon(Main.class.getResource("../images/startButtonBasic.png"));
 	private ImageIcon quitButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/quitButtonEntered.png"));
 	private ImageIcon quitButtonBasicImage = new ImageIcon(Main.class.getResource("../images/quitButtonBasic.png"));
-	
-	//UI 변수
+	private ImageIcon leftButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/leftButtonEntered.png"));
+	private ImageIcon leftButtonBasicImage = new ImageIcon(Main.class.getResource("../images/leftButtonBasic.png"));
+	private ImageIcon rightButtonEnteredImage = new ImageIcon(Main.class.getResource("../images/rightButtonEntered.png"));
+	private ImageIcon rightButtonBasicImage = new ImageIcon(Main.class.getResource("../images/rightButtonBasic.png"));
+
+	// UI 변수
+	private Image titleImage = new ImageIcon(Main.class.getResource("../images/Love Mode Title Image.png")).getImage();
 	private Image selectedImage = new ImageIcon(Main.class.getResource("../images/Love Mode Start Image.png")).getImage();
 	private Image background = new ImageIcon(Main.class.getResource("../images/introBackground(title).jpg")).getImage();
 	private JLabel menuBar = new JLabel(new ImageIcon(Main.class.getResource("../images/menuBar.png")));
-	
-	//버튼 변수
+
+	// 버튼 변수
 	private JButton exitButton = new JButton(exitButtonBasicImage);
 	private JButton startButton = new JButton(startButtonBasicImage);
-	private JButton quitButton = new JButton(quitButtonBasicImage);	
-	
-	//마우스 좌표
+	private JButton quitButton = new JButton(quitButtonBasicImage);
+	private JButton leftButton = new JButton(leftButtonBasicImage);
+	private JButton rightButton = new JButton(rightButtonBasicImage);
+
+	// 마우스 좌표
 	private int mouseX, mouseY;
-	
+
 	private boolean isMainScreen = false;
 
 	public DynamicBeat() {
@@ -50,26 +58,28 @@ public class DynamicBeat extends JFrame {
 		setVisible(true);
 		setBackground(new Color(0, 0, 0, 0));
 		setLayout(null);
-		
-		//setting exitButton
+
+		// setting exitButton
 		exitButton.setBounds(1245, 0, 30, 30);
 		exitButton.setBorderPainted(false);
 		exitButton.setContentAreaFilled(false);
 		exitButton.setFocusPainted(false);
 		exitButton.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseEntered(MouseEvent e){
+			public void mouseEntered(MouseEvent e) {
 				exitButton.setIcon(exitButtonEnteredImage);
 				exitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
+
 			@Override
-			public void mouseExited(MouseEvent e){
+			public void mouseExited(MouseEvent e) {
 				exitButton.setIcon(exitButtonBasicImage);
 				exitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
+
 			@Override
-			public void mousePressed(MouseEvent e){
-				
+			public void mousePressed(MouseEvent e) {
+
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e1) {
@@ -79,53 +89,59 @@ public class DynamicBeat extends JFrame {
 			}
 		});
 		add(exitButton);
-		
-		//setting startButton
+
+		// setting startButton
 		startButton.setBounds(840, 440, 400, 100);
 		startButton.setBorderPainted(false);
 		startButton.setContentAreaFilled(false);
 		startButton.setFocusPainted(false);
 		startButton.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseEntered(MouseEvent e){
+			public void mouseEntered(MouseEvent e) {
 				startButton.setIcon(startButtonEnteredImage);
 				startButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
+
 			@Override
-			public void mouseExited(MouseEvent e){
+			public void mouseExited(MouseEvent e) {
 				startButton.setIcon(startButtonBasicImage);
 				startButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
+
 			@Override
-			public void mousePressed(MouseEvent e){
-				//게임 시작 이벤트
+			public void mousePressed(MouseEvent e) {
+				// 게임 시작 이벤트
 				startButton.setVisible(false);
 				quitButton.setVisible(false);
+				leftButton.setVisible(true);
+				rightButton.setVisible(true);
 				background = new ImageIcon(Main.class.getResource("../images/mainBackground.jpg")).getImage();
-				
+
 				isMainScreen = true;
 			}
 		});
 		add(startButton);
-		
-		//setting quitButton
+
+		// setting quitButton
 		quitButton.setBounds(840, 570, 400, 100);
 		quitButton.setBorderPainted(false);
 		quitButton.setContentAreaFilled(false);
 		quitButton.setFocusPainted(false);
 		quitButton.addMouseListener(new MouseAdapter() {
 			@Override
-			public void mouseEntered(MouseEvent e){
+			public void mouseEntered(MouseEvent e) {
 				quitButton.setIcon(quitButtonEnteredImage);
 				quitButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 			}
+
 			@Override
-			public void mouseExited(MouseEvent e){
+			public void mouseExited(MouseEvent e) {
 				quitButton.setIcon(quitButtonBasicImage);
 				quitButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 			}
+
 			@Override
-			public void mousePressed(MouseEvent e){
+			public void mousePressed(MouseEvent e) {
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e1) {
@@ -135,10 +151,62 @@ public class DynamicBeat extends JFrame {
 			}
 		});
 		add(quitButton);
-		
-		//메뉴바 세팅
+
+		// setting leftButton
+		leftButton.setVisible(false);
+		leftButton.setBounds(140, 310, 60, 60);
+		leftButton.setBorderPainted(false);
+		leftButton.setContentAreaFilled(false);
+		leftButton.setFocusPainted(false);
+		leftButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				leftButton.setIcon(leftButtonEnteredImage);
+				leftButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				leftButton.setIcon(leftButtonBasicImage);
+				leftButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				//왼쪽 버튼 이벤트
+			}
+		});
+		add(leftButton);
+
+		// setting rightButton
+		rightButton.setVisible(false);
+		rightButton.setBounds(1080, 310, 60, 60);
+		rightButton.setBorderPainted(false);
+		rightButton.setContentAreaFilled(false);
+		rightButton.setFocusPainted(false);
+		rightButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				rightButton.setIcon(rightButtonEnteredImage);
+				rightButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				rightButton.setIcon(rightButtonBasicImage);
+				rightButton.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				//오른쪽 버튼 이벤트
+			}
+		});
+		add(rightButton);
+
+		// 메뉴바 세팅
 		menuBar.setBounds(0, 0, 1280, 30);
-		
+
 		menuBar.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
@@ -146,7 +214,7 @@ public class DynamicBeat extends JFrame {
 				mouseY = e.getY();
 			}
 		});
-		
+
 		menuBar.addMouseMotionListener(new MouseAdapter() {
 			@Override
 			public void mouseDragged(MouseEvent e) {
@@ -155,7 +223,7 @@ public class DynamicBeat extends JFrame {
 				setLocation(x - mouseX, y - mouseY);
 			}
 		});
-		
+
 		add(menuBar);
 
 		Music introMusic = new Music("introMusic.mp3", true);
@@ -171,11 +239,12 @@ public class DynamicBeat extends JFrame {
 
 	public void screenDraw(Graphics g) {
 		g.drawImage(background, 0, 0, null);
-		if(isMainScreen) {
+		if (isMainScreen) {
 			g.drawImage(selectedImage, 340, 100, null);
+			g.drawImage(titleImage, 340, 70, null);
 		}
 		paintComponents(g);
 		this.repaint();
 	}
-	
+
 }
